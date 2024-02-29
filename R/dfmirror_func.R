2a6d9c2ddea5cc1e75a8069f8061512fd62be648
jacobpstein <jacobpstein@gmail.com>
2024-02-28 21:11:31 -0500

    Added hide_cols option

--- R/dfmirror_func.R
+++ R/dfmirror_func.R
@@ -3,6 +3,7 @@
 #' @param input_df A data frame.
 #' @param num_obs A numeric value specifying how many observations (i.e., rows) to simulate.
 #' @param columns_to_simulate One or more columns from the `input_df` to simulate. The default is all columns.
+#' @param hide_cols Select `TRUE` to replace column names with anonymized names that take the form of "v1," "v2," and so on for each column.
 #' @return A list with
 #' * `simulated_df` a data frame object containing a simulated mirror of the input df where each specified column has the same mean and standard deviation as the input df
 #' * `code` a string vector that can be run in conjunction with `cat()` to output easily shareable code to recreate `simulated_df`
@@ -29,10 +30,7 @@
 #' cat(mirrored_df$code)
 
 
-
-simulate_dataframe <- function(input_df, num_obs = 1, columns_to_simulate = colnames(input_df)) {
-
-
+simulate_dataframe <- function(input_df, num_obs = 1, columns_to_simulate = colnames(input_df), hide_cols = FALSE) {
   simulate_skewed_gamma <- function(column_data, num_obs) {
     # Custom function to simulate skewed gamma distribution while maintaining mean and standard deviation
     mean_val <- mean(column_data)
@@ -44,43 +42,42 @@ simulate_dataframe <- function(input_df, num_obs = 1, columns_to_simulate = coln
   }
 
   simulated_df <- data.frame(matrix(NA, nrow = num_obs, ncol = length(columns_to_simulate)))
-  colnames(simulated_df) <- columns_to_simulate
+  if (hide_cols) {
+    colnames(simulated_df) <- paste0("v", seq_along(columns_to_simulate))
+  } else {
+    colnames(simulated_df) <- columns_to_simulate
+  }
 
   code <- "simulated_df <- data.frame(\n"
 
-  for (col in columns_to_simulate) {
+  for (i in seq_along(columns_to_simulate)) {
+    col <- columns_to_simulate[i]
     if (is.numeric(input_df[[col]])) {
-      # Check for normality using the Shapiro-Wilk test
       shapiro_test <- shapiro.test(input_df[[col]])
 
       if (shapiro_test$p.value > 0.05 && length(unique(input_df[[col]])) > 1) {
-        # If p-value > 0.05 and there is variation, assume normal distribution
-        dist_fit <- fitdist(input_df[[col]], "norm")
-        simulated_df[[col]] <- rnorm(num_obs, mean = dist_fit$estimate[1], sd = dist_fit$estimate[2])
-        code <- paste0(code, "  ", col, " = rnorm(", num_obs, ", mean = ", dist_fit$estimate[1], ", sd = ", dist_fit$estimate[2], "),\n")
+        dist_fit <- fitdistrplus::fitdist(input_df[[col]], "norm")
+        simulated_df[[i]] <- rnorm(num_obs, mean = dist_fit$estimate[1], sd = dist_fit$estimate[2])
+        code <- paste0(code, "  v", i, " = rnorm(", num_obs, ", mean = ", dist_fit$estimate[1], ", sd = ", dist_fit$estimate[2], "),\n")
       } else {
-        # If p-value <= 0.05 or lacks variation, assume non-normal distribution
-        # Impute missing values before checking skewness
         input_df[[col]][is.na(input_df[[col]])] <- mean(input_df[[col]], na.rm = TRUE)
-        skew_value <- skewness(input_df[[col]])
+        skew_value <- e1071::skewness(input_df[[col]])
 
         if (!is.na(skew_value) && abs(skew_value) > 1) {
-          # If skewness is greater than 1, simulate skewed data using custom function
-          simulated_df[[col]] <- simulate_skewed_gamma(input_df[[col]], num_obs)
-          code <- paste0(code, "  ", col, " = simulate_skewed_gamma(input_df[['", col, "']], ", num_obs, "),\n")
+          simulated_df[[i]] <- simulate_skewed_gamma(input_df[[col]], num_obs)
+          code <- paste0(code, "  v", i, " = simulate_skewed_gamma(input_df[,", which(colnames(input_df) == col), "], ", num_obs, "),\n")
         } else {
-          # If skewness is not significant or missing, simulate as before
           sampled_values <- unique(input_df[[col]])
-          simulated_df[[col]] <- sample(sampled_values, num_obs, replace = TRUE)
-          code <- paste0(code, "  ", col, " = sample(c(", paste(sampled_values, collapse = ", "), "), ", num_obs, ", replace = TRUE),\n")
+          simulated_df[[i]] <- sample(sampled_values, num_obs, replace = TRUE)
+          code <- paste0(code, "  v", i, " = sample(c(", paste(sampled_values, collapse = ", "), "), ", num_obs, ", replace = TRUE),\n")
         }
       }
     } else if (is.factor(input_df[[col]])) {
-      simulated_df[[col]] <- factor(sample(levels(input_df[[col]]), num_obs, replace = TRUE))
-      code <- paste0(code, "  ", col, " = factor(sample(c(", paste(levels(input_df[[col]]), collapse = ", "), "), ", num_obs, ", replace = TRUE)),\n")
+      simulated_df[[i]] <- factor(sample(levels(input_df[[col]]), num_obs, replace = TRUE))
+      code <- paste0(code, "  v", i, " = factor(sample(c(", paste(levels(input_df[[col]]), collapse = ", "), "), ", num_obs, ", replace = TRUE)),\n")
     } else if (is.character(input_df[[col]])) {
-      simulated_df[[col]] <- replicate(num_obs, paste(sample(letters, 10, replace = TRUE), collapse = ""))
-      code <- paste0(code, "  ", col, " = replicate(", num_obs, ", paste(sample(letters, 10, replace = TRUE), collapse = '')),\n")
+      simulated_df[[i]] <- replicate(num_obs, paste(sample(letters, 10, replace = TRUE), collapse = ""))
+      code <- paste0(code, "  v", i, " = replicate(", num_obs, ", paste(sample(letters, 10, replace = TRUE), collapse = '')),\n")
     }
   }
 
@@ -89,5 +86,4 @@ simulate_dataframe <- function(input_df, num_obs = 1, columns_to_simulate = coln
 
   return(list(simulated_df = simulated_df, code = code))
 }
-
 
